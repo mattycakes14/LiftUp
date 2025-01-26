4ba0acad011db2a3617d4487deec07d3ffe0cc5d
mattycakes14 <mlau191@uw.edu>
2025-01-25 18:52:48 -0800

    Test

--- front-end/app/tabs/Profile.tsx
+++ front-end/app/tabs/Profile.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView, Text, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
+import axios from "axios";
 const Profile = () => {
+  //navigation
   const navigation = useNavigation();
   const handleLogout = () => {
     navigation.navigate("index");
+  };
+
+  //test
+  const fetchData = async () => {
+    axios
+      .get("http://192.168.1.10:5000/users")
+      .then((response) => {
+        console.log(response.data); // Handle the user data
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   };
   return (
     <SafeAreaView>
       <Text>This is the profile tab</Text>
       <Button title="Logout" onPress={handleLogout} />
+      <Button title="Get Data" onPress={fetchData} />
     </SafeAreaView>
   );
 };
